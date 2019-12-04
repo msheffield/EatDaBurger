12ef0991d2c4bea8b18cb3594b63d2dbf973fdd7
msheffield <msheffield@adaptivebiotech.com>
2019-12-04 14:52:52 -0800

    update devoured state complete

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,80 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
 var orm = {
-    all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput +";";
-        
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+
         connection.query(queryString, function (err, data) {
             if (err) throw err;
 
             cb(data);
+        });
+    },
+    // Create a new burger in the database
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // Update an existing burger in the database (i.e. change devoured state)
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        queryString += ";";
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
     }
 }
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,18 +1,40 @@
 $(function () {
     $(".eat-or-uneat").on("click", function (event) {
         var id = $(this).data("id");
-        var burgerState = $(this).data("burgerState");
+        var devouredState = $(this).data("burgerstate");
 
         var newBurgerState = {
-            devoured: burgerState
+            devoured: devouredState
         };
+
 
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newBurgerState
         }).then(
             function () {
-                console.log("Changed devoured to" + burgerState);
+                console.log("Changed devoured to" + devouredState);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#new-burger").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        console.log("new burger:", newBurger);
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created a new burger");
                 location.reload();
             }
         );
